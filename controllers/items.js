16178f04e25943b104b5e3797d5a4fd8e0529461
dazzoc <dazzo.chris@gmail.com>
2022-01-07 16:06:10 -0600

    seed data

--- controllers/items.js
+++ controllers/items.js
@@ -2,17 +2,55 @@
 const express = require('express');
 const itemsRouter = express.Router(); 
 const Item = require('../models/item');
+const itemSeed = require('../models/itemSeed')
 
 //==========================================================
 // ROUTES
-// Index
-itemsRouter.get('/', (req, res) => {
-    res.send('HELLO')
-    // Item.find({}, (error, allItems) => {
-    //     res.render('index', {
-    //         items: allItems,
-    //     });
-    // });
+// Seed - Route
+itemsRouter.get('/items/seed', (req, res) => {
+    Item.deleteMany({}, (error, allItems) => {});
+    Item.create(itemSeed, (error, data) => {
+        res.redirect('/items');
+    });
 });
 
+// Index - Route
+itemsRouter.get('/', (req, res) => {
+    //res.send('HELLO')
+    Item.find({}, (error, allItems) => {
+        res.render('index', {
+            items: allItems,
+        });
+    });
+});
+
+// New - Route
+itemsRouter.get('/new', (req, res) => {
+    res.render('/new');
+});
+
+// Detele - Route
+
+// Update - Route
+
+// Create - Route
+itemsRouter.post('/', (req, res) => {
+    Item.create(req.body, (error, items) => {
+        res.redirect('/items');
+    });
+});
+
+// Edit - Route
+
+// Show - Route
+itemsRouter.get('/:id', (req, res) => {
+    Item.findById(req.params.id, (err, foundItem) => {
+        res.render('/show', {
+            item: foundItem,
+        });
+    });
+});
+
+//=============================================================
+// EXPORT
 module.exports = itemsRouter;
---
+++ models/itemSeed.js
@@ -0,0 +1,21 @@
+module.exports = [{
+    title: 'Give the Dog a BATH',
+    description: 'Damn Dog got skunked again! wash the dog in tomato juice and vinigar.',
+    dueDate: 1/14/22,
+    urget: true,
+}, {
+    title: 'Hang Pictures',
+    description: 'Hand up the 5 pictures Mom sent in the mail for chrismas in the living room. If she dosent see them next time shes over shes going to have a fit.',
+    dueDate: 1/20/22,
+    urget: false,
+}, {
+    title: 'Clean Kitchen',
+    description: 'Do the Dishes, put dishes away, cleanout the Junk drawer, scrub the counters, windex the windows.',
+    dueDate: 1/14/22,
+    urget: true,
+}, {
+    title: 'Loose Change',
+    description: 'Take the loose change to the bank for extra snack money',
+    dueDate: 1/30/22,
+    urget: false,
+}]
