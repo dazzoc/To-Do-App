c65d858097a144298bee1bda93ad4e770fc03f1a
dazzoc <dazzo.chris@gmail.com>
2022-01-12 16:12:32 -0600

    update for heroku

--- public/script.js
+++ public/script.js
@@ -1,6 +1,9 @@
 // VARIABLES
 const nav = document.querySelector('#head');
 let userScrollY = window.scrollY;
+const openModelButton = document.querySelectorAll('#model-trash');
+const closeModelButton = document.querySelectorAll('#model-close-btn');
+const overlay = document.getElementById('overlay');
 
 
 // EVENT LISTENERS
@@ -13,3 +16,29 @@ window.addEventListener('scroll', () => {
     userScrollY = window.scrollY;
 });
 
+// FUNCTIONS
+openModelButton.forEach(button => {
+    button.addEventListener('click', () => {
+        const model = document.querySelector(button.dataset.modelTarget);
+        openModel(model);
+    });
+});
+
+closeModelButton.forEach(button => {
+    button.addEventListener('click', () => {
+        const model = button.closest('.model');
+        closeModel(model);
+    });
+});
+
+function openModel(model) {
+    if(model === null) return
+    model.classList.add('active');
+    overlay.classList.add('active');
+};
+
+function closeModel(model) {
+    if(model === null) return
+    model.classList.remove('active');
+    overlay.classList.remove('active');
+};
