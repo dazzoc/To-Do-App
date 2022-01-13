c7135d5fbf282e0539445517eeacb54b682c7a06
dazzoc <dazzo.chris@gmail.com>
2022-01-12 18:24:14 -0600

    deleted modal

--- public/script.js
+++ public/script.js
@@ -1,9 +1,6 @@
 // VARIABLES
 const nav = document.querySelector('#head');
 let userScrollY = window.scrollY;
-const openModelButton = document.querySelectorAll('#model-trash');
-const closeModelButton = document.querySelectorAll('#model-close-btn');
-const overlay = document.getElementById('overlay');
 
 
 // EVENT LISTENERS
@@ -16,29 +13,3 @@ window.addEventListener('scroll', () => {
     userScrollY = window.scrollY;
 });
 
-// FUNCTIONS
-openModelButton.forEach(button => {
-    button.addEventListener('click', () => {
-        const model = document.querySelector(button.dataset.modelTarget);
-        openModel(model);
-    });
-});
-
-closeModelButton.forEach(button => {
-    button.addEventListener('click', () => {
-        const model = button.closest('.model');
-        closeModel(model);
-    });
-});
-
-function openModel(model) {
-    if(model === null) return
-    model.classList.add('active');
-    overlay.classList.add('active');
-};
-
-function closeModel(model) {
-    if(model === null) return
-    model.classList.remove('active');
-    overlay.classList.remove('active');
-};
